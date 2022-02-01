b88836f1f7c109fd019fd6f9c69c2d7c0f379dac
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-01 15:56:01 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,50 +4,30 @@
 
 struct Struct_ColourCode
 {
-    char majorColour[25];
-    char minorColour[25];  
+    const char *majorColor;
+    const char *minorColor;  
     char colourCode;
 };
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-int printColorMapStub(void (*Fn_Ptr_printColourPair)(int,int,  char*,  char* ));
+int printColorMap(void (*Fn_Ptr_printColourPairStub)(Struct_ColourCode));
 Struct_ColourCode CopyColourInfo(int majorColourIndex, int minorColourIndex);
-void printColourPairAndCodeStub(int colorCode, char* majorColor, char* minorColor);
+void printColourPairStub(Struct_ColourCode);
  
-int ColorCodePairNoToBePrintedInConsole = 1;
 
 // Source Code 
-int printColorMap() {
+int printColorMap(void (*Fn_Ptr_printColourPairStub)(ColorPair)) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printColourPairStub(CopyColourInfo(i,j));
         }
     }
     return i * j;
-}
-
-// Dependency Injection
-int printColorMapStub(void (*Fn_Ptr_printColourPair)(int,int, char*,  char* ))
-{
-
-    int majorColourIndex = 0; 
-    int minorColourIndex = 0;
-    Struct_ColourCode ColourInfo;
-    for(majorColourIndex = 0; majorColourIndex < 5; majorColourIndex++) 
-    {
-        for(minorColourIndex = 0; minorColourIndex < 5; minorColourIndex++) 
-        {
-            ColourInfo = CopyColourInfo(majorColourIndex,minorColourIndex);
-            Fn_Ptr_printColourPair(majorColourIndex,minorColourIndex,ColourInfo.majorColour,ColourInfo.minorColour);
-        }
-    }
-
-    return (minorColourIndex * majorColourIndex);
 }
 
 Struct_ColourCode CopyColourInfo(int majorColourIndex, int minorColourIndex)
@@ -60,26 +40,16 @@ Struct_ColourCode CopyColourInfo(int majorColourIndex, int minorColourIndex)
     return ColourInfo;   
 }
 
-void printColourPairStub(int majorColourIndex, int minorColourIndex,  char* majorColor,  char* minorColor)
+void printColourPairStub(Struct_ColourCode ColorPairReceived)
 {
-    printf("%d | %s | %s\n", majorColourIndex * 5 + minorColourIndex, majorColor, minorColor);
-    ColorCodePairNoToBePrintedInConsole++;
+    printf("%d | %s | %s\n", ColorPairReceived.colourCode, ColorPairReceived.majorColor, ColorPairReceived.minorColor);
 }
 
 
 int main() {
-    Struct_ColourCode test_colourInfo;
-    int test_majorColourIndex = 0;
-    int test_minorColourIndex = 0;
-    int test_expectedColourCode = 0;
-    void (*Fn_Ptr)(int,int,  char*,  char*) = printColourPairStub;
-    test_colourInfo = CopyColourInfo(test_majorColourIndex,test_minorColourIndex);
-    test_expectedColourCode = (test_majorColourIndex * 5 + test_minorColourIndex);
-    assert(test_colourInfo.colourCode == test_expectedColourCode);
-    assert(strcmp(test_colourInfo.majorColour,majorColor[test_majorColourIndex]) == 0);
-    assert(strcmp(test_colourInfo.minorColour,minorColor[test_minorColourIndex]) == 0);
-    int result = printColorMapStub(Fn_Ptr);
-    assert(result == ColorCodePairNoToBePrintedInConsole);
+    void (*Fn_Ptr)(Struct_ColourCode) = printColourPairStub;
+    int result = printColorMap(Fn_Ptr);
+    assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
 }
