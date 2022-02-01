ea38e13ef40270849083fe8ffcd4731a4fa4ef6c
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-01 16:34:55 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -5,12 +5,10 @@
 #define MIN_THRESHOLD 150
     
 int alertFailureCount = 0;
-int checkPerformedCount = 0;
 int checkFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    checkPerformedCount +=1;
     // Return 200 for ok
     // Return 500 for not-ok
     if(celcius >= MIN_THRESHOLD && celcius <=MAX_THRESHOLD)
@@ -32,14 +30,13 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
-    assert(checkPerformedCount == 2);
     assert(alertFailureCount == checkFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
